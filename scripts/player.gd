6f2d6dffc866417bbc168e61327fe250b84add5c
ebistu <127283670+ebistu@users.noreply.github.com>
2023-12-20 20:36:48 +0200

    working on all scripts

--- scripts/player.gd
+++ scripts/player.gd
@@ -17,10 +17,10 @@ func _process(delta):
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
 	if Input.is_action_pressed("move_up"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_down"):
 		velocity.y -= 1
-	if velocity.lenght() > 0:
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite2D.play()
 	else:
@@ -32,13 +32,13 @@ func _process(delta):
 		$AnimatedSprite2D.animation = "walk"
 		$AnimatedSprite2D.flip_v = false
 		$AnimatedSprite2D.flip_h = velocity.x < 0
-	elif velocity.y != 0:
+	#elif velocity.y != 0:
 		#$AnimatedSprite2D.animation = "up"
-		$AnimatedSprite2D.flip_v = velocity.y > 0
+		#$AnimatedSprite2D.flip_v = velocity.y > 0
 		
 
 
-func _on_body_entered(body):
+func _on_body_entered(_body):
 	hide()
 	hit.emit()
 	$CollisionShape2D.set_deferred("disabled", true)

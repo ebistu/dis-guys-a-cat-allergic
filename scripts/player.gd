40c4caab443706a6590b99ca610ff57152316d14
ebistu <127283670+ebistu@users.noreply.github.com>
2023-12-21 17:53:09 +0200

    changed speed

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 signal hit
 var screen_size
-var speed = 400
+var speed = 200
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
